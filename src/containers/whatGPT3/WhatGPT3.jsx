e5e8ae61f555d8787a64a3262767ce8ce24d4670
ManausK <1905246@kiit.ac.in>
2023-04-25 23:39:27 +0530

    What is GPT3 done

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -6,13 +6,30 @@ const WhatGPT3 = () => {
   return (
     <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
       <div className="gpt3__whatgpt3-feature">
-        <Feature />
+        <Feature
+          title="What is GPT3"
+          text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."
+        />
       </div>
       <div className="gpt3__whatgpt3-heading">
-        <h1 className="gradient-text">
+        <h1 className="gradient__text">
           The possibilities are beyond your imagination
         </h1>
         <p>Explore The Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Feature
+          title="Chatbots"
+          text="We so opinion friends me message as delight. Whole front do of plate heard oh ought."
+        />
+        <Feature
+          title="Knowledgebase"
+          text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments ber."
+        />
+        <Feature
+          title="Education"
+          text="Change done kya tha and kuch bhi so nahi hain. Anything bachelor trance to on by. As put possible own shipdesk adding that."
+        />
       </div>
     </div>
   );
